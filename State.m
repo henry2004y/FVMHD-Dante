eca98d7da5047d35b885892f797800c7dfc50a63
hyzhou <hyzhou@umich.edu>
2019-01-01 11:58:42 -0500

    Modified plotting and Riemann problem setup.

--- State.m
+++ State.m
@@ -1,4 +1,4 @@
-classdef State < handle
+classdef State %< handle
    %STATE The class of physical states in MHD.
    %   Primitive variables include:
    % rho: density
@@ -6,6 +6,9 @@ classdef State < handle
    % B:   magnetic field (3D)
    % p:   pressure
    % e:   energy
+   %
+   % For higher order schemes, this should be a normal class instead of
+   % handle class.
    
    %======================== MEMBERS =================================
    properties
@@ -31,8 +34,9 @@ classdef State < handle
          disp('Initializing state variables...')
          
          % Set initial conditions
-         [density,velocity,Bfield,pressure] = obj.set_init;
-         %[density,velocity,Bfield,pressure,tEnd] = set_init_Riemann(grid);
+         %[density,velocity,Bfield,pressure] = obj.set_init;
+         [density,velocity,Bfield,pressure,tEnd] = ...
+            obj.set_init_Riemann(grid);
          
          if nargin == 0
             disp('Using default grid')
@@ -76,7 +80,7 @@ classdef State < handle
          end
       end
       
-      function [Rho,U,B,P,tEnd] = set_init_Riemann(grid)
+      function [Rho,U,B,P,tEnd,CFL] = set_init_Riemann(grid)
          %SET_INIT_RIEMANN Load the IC of a classical 1D Riemann Problems.
          %   Detailed explanation goes here
          
@@ -88,105 +92,105 @@ classdef State < handle
                p   = [1    0.1  ];
                u   = [0    0    ];
                rho = [1    0.125];
-               tEnd = 0.1; cfl = 0.90;
+               tEnd = 0.1; CFL = 0.90;
                
             case{2} % Configuration 2, Left Expansion and right strong shock
                fprintf('Case 2: Left Expansion and right strong shock \n');
                p   = [1000 0.1  ];
                u   = [0    0    ];
                rho = [3    2    ];
-               tEnd = 0.02; cfl = 0.90;
+               tEnd = 0.02; CFL = 0.90;
                
             case{3} % Configuration 3, Right Expansion and left strong shock
                fprintf('Case 3: Right Expansion and left strong shock \n');
                p   = [7    10   ];
                u   = [0    0    ];
                rho = [1    1    ];
-               tEnd = 0.1; cfl = 0.90;
+               tEnd = 0.1; CFL = 0.90;
                
             case{4} % Configuration 4, Double Shock
                fprintf('Case 4: Double Shock \n');
                p   = [450  45   ];
                u   = [20   -6   ];
                rho = [6    6    ];
-               tEnd = 0.02; cfl = 0.90;
+               tEnd = 0.02; CFL = 0.90;
                
             case{5} % Configuration 5, Double Expansion
                fprintf('Case 5: Double Expansion \n');
                p   = [40   40   ];
                u   = [-2   2    ];
                rho = [1    2.5  ];
-               tEnd = 0.03; cfl = 0.90;
+               tEnd = 0.03; CFL = 0.90;
                
             case{6} % Configuration 6, Cavitation
                fprintf('Case 6: Cavitation \n');
                p   = [0.4  0.4  ];
                u   = [-2    2   ];
                rho = [ 1    1   ];
-               tEnd = 0.1; cfl = 0.90;
+               tEnd = 0.1; CFL = 0.90;
                
             case{7} % Shocktube problem of G.A. Sod, JCP 27:1, 1978
                fprintf('Shocktube problem of G.A. Sod, JCP 27:1, 1978');
                p   = [1.0  0.1  ];
                u   = [0.75 0    ];
                rho = [1    0.125];
-               tEnd = 0.17; cfl = 0.90;
+               tEnd = 0.17; CFL = 0.90;
                
             case{8} % Lax test case: M. Arora and P.L. Roe: JCP 132:3-11, 1997
                fprintf('Lax test case: M. Arora and P.L. Roe: JCP 132:3-11, 1997');
                p   = [3.528 0.571];
                u   = [0.698 0    ];
                rho = [0.445 0.5  ];
-               tEnd = 0.15; cfl = 0.90;
+               tEnd = 0.15; CFL = 0.90;
                
             case{9} % Mach = 3 test case: M. Arora and P.L. Roe: JCP 132:3-11, 1997
                fprintf('Mach = 3 test case: M. Arora and P.L. Roe: JCP 132:3-11, 1997');
                p   = [10.333  1  ];
                u   = [ 0.92  3.55];
                rho = [ 3.857  1  ];
-               tEnd = 0.09; cfl = 0.90;
+               tEnd = 0.09; CFL = 0.90;
                
             case{10} % Shocktube problem with supersonic zone
                fprintf('Shocktube problem with supersonic zone');
                p   = [1  0.02];
                u   = [0  0.00];
                rho = [1  0.02];
-               tEnd = 0.162; cfl = 0.90;
+               tEnd = 0.162; CFL = 0.90;
                
             case{11} % Contact discontinuity
                fprintf('Contact discontinuity');
                p   = [0.5 0.5];
                u   = [0.0 0.0];
                rho = [1.0 0.6];
-               tEnd = 1; cfl = 0.90;
+               tEnd = 1; CFL = 0.90;
                
             case{12} % Stationary shock
                fprintf('Stationary shock');
                p   = [ 1.0  0.1 ];
                u   = [-2.0 -2.0 ];
                rho = [ 1.0 0.125];
-               tEnd = 0.1; cfl = 0.28;
+               tEnd = 0.1; CFL = 0.28;
                
             case{13} % left side of 2-d Riemman case 12
                fprintf('left side of 2-d Riemman case 13');
                p   = [ 1.0  1.0 ];
                u   = [ 0.0  0.0 ];
                rho = [ 0.8  1.0 ];
-               tEnd = 0.1; cfl = 0.25;
+               tEnd = 0.1; CFL = 0.25;
                
             case{14} % right side of 2-d Riemman case 12
                fprintf('right side of 2-d Riemman case 14');
                p   = [ 1.0  0.4 ];
                u   = [ 0.7276  0.0 ];
                rho = [ 1.0  0.5313 ];
-               tEnd = 0.1; cfl = 0.25;
+               tEnd = 0.1; CFL = 0.25;
                
             case{15} % Stationary Shock
                fprintf('right side of 2-d Riemman case 15');
                p   = [ 0.1  0.676 ];
                u   = [ 1.2  0.723966942148760 ];
                rho = [ 1.0  1.657534246575342 ];
-               tEnd = 0.1; cfl = 0.25;
+               tEnd = 0.1; CFL = 0.25;
                
             otherwise
                error('Case not available');
@@ -263,7 +267,7 @@ classdef State < handle
             0.5*obj.Rho.*sum(obj.U.^2,4) + 0.5*sum(obj.B.^2,4);
       end
       
-      function update_state(obj,grid,faceFlux,source,time)
+      function obj = update_state(obj,grid,faceFlux,source,time)
          %UPDATE_STATE Update the state variables in one timestep.
          %
          %INPUTS:
@@ -313,16 +317,16 @@ classdef State < handle
                   CellSize_D(2) +...
                   (Flux_ZV(:,:,2:end,:) - Flux_ZV(:,:,1:end-1,:))/...
                   CellSize_D(3))+...
-                  source_VG;
+                  source_GV;
             else
                stateNew_GV(iMin:iMax,jMin:jMax,kMin:kMax,Rho_:Bz_) = ...
                   state_GV(iMin:iMax,jMin:jMax,kMin:kMax,Rho_:Bz_) - ...
                   dt.*(...
-                  (Flux_XV(2:end,:,:,Rho_:Bz_) -...
+                  (Flux_XV(2:end  ,:,:,Rho_:Bz_) -...
                    Flux_XV(1:end-1,:,:,Rho_:Bz_)) / CellSize_D(1) + ...
-                  (Flux_YV(:,2:end,:,Rho_:Bz_) - ...
+                  (Flux_YV(:,2:end  ,:,Rho_:Bz_) - ...
                    Flux_YV(:,1:end-1,:,Rho_:Bz_)) / CellSize_D(2) + ...
-                  (Flux_ZV(:,:,2:end,Rho_:Bz_) - ...
+                  (Flux_ZV(:,:,2:end  ,Rho_:Bz_) - ...
                    Flux_ZV(:,:,1:end-1,Rho_:Bz_)) / CellSize_D(3))+ ...
                   source_GV(:,:,:,Rho_:Bz_);
                
@@ -373,25 +377,31 @@ classdef State < handle
          kMax = Parameters.kMax;
          
          varname = varargin{1};
-         iStep = varargin{2};
+         grid    = varargin{2};
+         iStep   = varargin{3};
          
          switch varname
             case 'rho'
-               y = obj.Rho;
+               var = obj.Rho;
             case 'ux'
-               y = obj.U(:,:,:,1);
+               var = obj.U(:,:,:,1);
+            case 'p'
+               var = obj.P;
             otherwise
                error('unknown plotting varname!')
          end
          
-         y = squeeze(y);
-         y = y(iMin:iMax,jMin:jMax,kMin:kMax);
+         x = grid.getX;
          
-         %figure;
-         plot(y,'LineWidth',1.5)
+         var = squeeze(var); % Unify dimensions
+         var = var(iMin:iMax,jMin:jMax,kMin:kMax); % Remove ghost cells
+         
+         figure(3)
+         plot(x,var,'LineWidth',1.5)
          
          title(sprintf('iStep=%d',iStep))
-         axis([1 101 0 2.0])
+         legend(varname)
+         %axis([1 201 0 2.0])
          
          %M(k) = getframe;
       end
