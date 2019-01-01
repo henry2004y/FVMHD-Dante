6fc08e5e9507b99b3b09754ab93739377f0500c7
hyzhou <hyzhou@umich.edu>
2019-01-01 11:55:58 -0500

    Added 2nd order scheme.

--- FaceValue.m
+++ FaceValue.m
@@ -24,6 +24,8 @@ classdef FaceValue < handle
    methods (Access = public)
       function calc_face_value(obj,state)
          %CALC_FACE_VALUE Calculate the face values.
+         % For uniform grid, there is no need to take grid sizes into
+         % account.
          
          state_GV = state.state_GV;
          
@@ -43,31 +45,109 @@ classdef FaceValue < handle
             obj.RState_ZV = state_GV(iMin:iMax,jMin:jMax,kMin:kMax+1,:);
          else % 2nd order
             % Compute and limit slopes
-            %dq_X=zeros(Parameters.nVar,Parameters.nI+1);
             
+            % Get slope with limiters
             switch Parameters.limiter
                case 'MC'
+                  % Find dq_j = minmod{fwd diff, bwd diff, cntrl diff}
+                  dqR_X = ...
+                     state_GV(iMin  :iMax+2,jMin:jMax,kMin:kMax,:) -...
+                     state_GV(iMin-1:iMax+1,jMin:jMax,kMin:kMax,:);
+                  dqL_X = ...
+                     state_GV(iMin-1:iMax+1,jMin:jMax,kMin:kMax,:) - ...
+                     state_GV(iMin-2:iMax  ,jMin:jMax,kMin:kMax,:);
+                  dqC_X = ...
+                     state_GV(iMin  :iMax+2,jMin:jMax,kMin:kMax,:) - ...
+                     state_GV(iMin-2:iMax  ,jMin:jMax,kMin:kMax,:);
                   
+                  dq_X = obj.minmod(dqR_X,dqL_X,dqC_X);
+                  
+                  dqR_Y = ...
+                     state_GV(iMin:iMax,jMin  :jMax+2,kMin:kMax,:) -...
+                     state_GV(iMin:iMax,jMin-1:jMax+1,kMin:kMax,:);
+                  dqL_Y = ...
+                     state_GV(iMin:iMax,jMin-1:jMax+1,kMin:kMax,:) - ...
+                     state_GV(iMin:iMax,jMin-2:jMax  ,kMin:kMax,:);
+                  
+                  dq_Y = obj.minmod(dqR_Y,dqL_Y,dqC_Y);
+                  
+                  dqR_Z = ...
+                     state_GV(iMin:iMax,jMin:jMax,kMin  :kMax+2,:) - ...
+                     state_GV(iMin:iMax,jMin:jMax,kMin-1:kMax+1,:);
+                  dqL_Z = ...
+                     state_GV(iMin:iMax,jMin:jMax,kMin-1:kMax+1,:) - ...
+                     state_GV(iMin:iMax,jMin:jMax,kMin-2:kMax  ,:);
+                  
+                  dq_Z = obj.minmod(dqR_Z,dqL_Z,dqC_Z);
                case 'MM' % Minmod limiter
                   % Find dq_j = minmod{fwd diff, bwd diff}
-                  dqR_X = squeeze(...
-                     state_GV(iMin+1:iMax+2,jMin:jMax,kMin:kMax,:) -...
-                     state_GV(iMin:iMax+1,jMin:jMax,kMin:kMax,:));
-                  dqL_X = squeeze(...
-                     state_GV(iMin:iMax+1,jMin:jMax,kMin:kMax,:) - ...
-                     state_GV(iMin-1:iMax,jMin:jMax,kMin:kMax,:));
                   
-                  dq_X = minmod(dqR_X,dqL_X);
+                  dqR_X = ...
+                     state_GV(iMin  :iMax+2,jMin:jMax,kMin:kMax,:) -...
+                     state_GV(iMin-1:iMax+1,jMin:jMax,kMin:kMax,:);
+                  dqL_X = ...
+                     state_GV(iMin-1:iMax+1,jMin:jMax,kMin:kMax,:) - ...
+                     state_GV(iMin-2:iMax  ,jMin:jMax,kMin:kMax,:);
+                  
+                  dq_X = obj.minmod(dqR_X,dqL_X);
+                  
+                  dqR_Y = ...
+                     state_GV(iMin:iMax,jMin  :jMax+2,kMin:kMax,:) -...
+                     state_GV(iMin:iMax,jMin-1:jMax+1,kMin:kMax,:);
+                  dqL_Y = ...
+                     state_GV(iMin:iMax,jMin-1:jMax+1,kMin:kMax,:) - ...
+                     state_GV(iMin:iMax,jMin-2:jMax  ,kMin:kMax,:);
+                  
+                  dq_Y = obj.minmod(dqR_Y,dqL_Y);
+                  
+                  dqR_Z = ...
+                     state_GV(iMin:iMax,jMin:jMax,kMin  :kMax+2,:) - ...
+                     state_GV(iMin:iMax,jMin:jMax,kMin-1:kMax+1,:);
+                  dqL_Z = ...
+                     state_GV(iMin:iMax,jMin:jMax,kMin-1:kMax+1,:) - ...
+                     state_GV(iMin:iMax,jMin:jMax,kMin-2:kMax  ,:);
+                  
+                  dq_Z = obj.minmod(dqR_Z,dqL_Z);
             end
             
-            % Get the gradient of states
-            
-            % Linear interpolation onto edge centers (with limiters)
+            % Linear interpolation onto edge centers
+            obj.LState_XV = state_GV(iMin-1:iMax,jMin:jMax,kMin:kMax,:)+...
+               0.5*dq_X(1:end-1,:,:,:);
+            obj.RState_XV = state_GV(iMin:iMax+1,jMin:jMax,kMin:kMax,:)-...
+               0.5*dq_X(2:end  ,:,:,:);
+            obj.LState_YV = state_GV(iMin:iMax,jMin-1:jMax,kMin:kMax,:)+...
+               0.5*dq_Y(:,1:end-1,:,:);
+            obj.RState_YV = state_GV(iMin:iMax,jMin:jMax+1,kMin:kMax,:)-...
+               0.5*dq_Y(:,2:end  ,:,:);
+            obj.LState_ZV = state_GV(iMin:iMax,jMin:jMax,kMin-1:kMax,:)+...
+               0.5*dq_Z(:,:,1:end-1,:);
+            obj.RState_ZV = state_GV(iMin:iMax,jMin:jMax,kMin:kMax+1,:)-...
+               0.5*dq_Z(:,:,2:end  ,:);
             
          end
       end
       
    end
    
-end
+   methods (Static)
+      function m = minmod(a,b,c)
+         %MINMOD
+	 % For three inputs, use Harten's generalized definition.
+         %OUTPUT:
+         % m: zero if opposite sign, otherwise the one of smaller magnitude.
 
+         if nargin == 2  % Two input arguments
+            m = (sign(a) + sign(b))/2.*min(abs(a),abs(b));
+         elseif nargin == 3  % Three input arguments
+	    s = (sign(a) + sign(b) + sign(c))/3;
+            if abs(s)==1
+	      m = s*min(abs(a),abs(b),abs(c));
+	    else 
+	      m = zeros(size(a));
+            end
+         end   
+      end
+            
+   end
+   
+end
